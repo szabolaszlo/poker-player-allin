1496809b32f93089ec71b3385552b6e4023aa98e
ngabesz-wse <gabor.nagy@innonic.com>
2019-02-02 10:50:12 +0100

    limp

--- player.php
+++ player.php
@@ -29,7 +29,7 @@ class Player
                 return 0;
             //case 'raise':
             case 'limp':
-                return (int)$player['stack'] * 0.3;
+                return 0;
             case 'fold':
                 return 0;
             default:
