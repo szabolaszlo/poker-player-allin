92e947a970f847fa9841e150bc55db995b9f2cb4
szabolaszlo <laszlo.szabo@innonic.com>
2019-02-02 10:10:40 +0100

    inteligent bet

--- player.php
+++ player.php
@@ -6,6 +6,7 @@ class Player
 
     public function betRequest($game_state)
     {
+        sleep(20);
         $bet = $game_state['current_buy_in'] - $game_state[$game_state['in_action']]['bet'];
         return max($bet, $game_state['small_blind']);
     }
